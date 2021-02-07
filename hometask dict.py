1b624fec5a7b4d3d96678bdb2e6e954fcd3b213b
Stanislav Azarov <77240491+spartaque5@users.noreply.github.com>
2021-02-07 18:41:13 +0000

    Add files via upload

---
+++ hometask dict.py
@@ -0,0 +1,11 @@
+#My old Phones
+phone1 = dict(brand="siemens", year=2004)
+phone2 = dict(brand="nokia" , year=2006)
+phone3 = dict(brand="sonyerickson" , year = 2008)
+phone4 = dict(brand="samsung" , year= 2010)
+myoldphones = {
+"phone1" : phone1,
+"phone2" : phone2,
+"phone3" : phone3,
+"phone4" : phone4}
+print(myoldphones)
